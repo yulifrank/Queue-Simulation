4bf6e65589bcc2d8876b54fe0b53d38f18b8e8ce
yulifrank <148456398+yulifrank@users.noreply.github.com>
2024-12-21 21:25:08 +0200

    add Customer Cashier and Queue classes

---
+++ Queue Simulation/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Simulation
+{
+    public class Customer
+    {
+        static int count = 1;
+        public int Id { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public double ServiceTime { get; set; }
+        public Customer( double serviceTime)
+        {
+            Id = count;
+            count += 1;
+            ArrivalTime = DateTime.Now; 
+            ServiceTime = serviceTime;
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
